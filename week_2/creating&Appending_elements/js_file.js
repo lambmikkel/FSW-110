a7f813ac71ca8c63beb517151a663f2cdb700429
lambmikkel <66314737+lambmikkel@users.noreply.github.com>
2020-08-08 21:52:58 -0700

    updated assignment 2

--- week_2/creating&Appending_elements/js_file.js
+++ week_2/creating&Appending_elements/js_file.js
@@ -4,8 +4,23 @@ var content = document.createTextNode('Welcome to my JS site');
 h1.appendChild(content);
 document.body.appendChild(h1);
 
-var newLi = document.createElement("li");
-var myList = document.getElementById("my_list");
-myList.append(newLi);
+//Paragraph Tag
+var para = document.createElement("p");
+var node = document.createTextNode("All of this was created with Javascript");
+para.appendChild(node);
+var element = document.getElementById("div1");
+element.appendChild(para);
 
 
+//List
+function createListItem(name) {
+  let li = document.createElement('li');
+  li.textContent = name;
+  return li;
+}
+// get the ul#menu
+const menu = document.querySelector('#list');
+// add menu item
+menu.appendChild(createListItem('Home'));
+menu.appendChild(createListItem('Services'));
+menu.appendChild(createListItem('About Us'));

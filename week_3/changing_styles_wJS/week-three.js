15753e3641351a9becec2ed2ff02e5f3e3091c56
lambmikkel <66314737+lambmikkel@users.noreply.github.com>
2020-08-17 06:55:12 -0700

    Update

--- week_3/changing_styles_wJS/week-three.js
+++ week_3/changing_styles_wJS/week-three.js
@@ -1,18 +1,20 @@
 (function makeHeader() {
   for (var i = 0; i < 5; i++) {
     var h2 = document.createElement("h2");
-      var content = document.createTextNode("Changing styles using JS");
-      h2.appendChild(content);
-      document.body.appendChild(h2);
-      // Appling styles on element
-      h2.style.color = "cornflowerblue";
-      h2.style.fontSize = "20px";
-      h2.style.fontWeight = "lighter";
-      h2.style.fontFamily = "sans-serif";
-      
+    var content = document.createTextNode("Changing styles using JS");
+    h2.appendChild(content);
+    document.body.appendChild(h2);
+    // Appling styles on element
+    h2.style.color = "cornflowerblue";
+    h2.style.fontSize = "20px";
+    h2.style.fontWeight = "lighter";
+    h2.style.fontFamily = "sans-serif";
 
-      }
-
+    var a = document.body;
+    if (a.classList) {
+      a.classList.add("border");
+    } else {
+      a.className += " border";
+    }
+  }
 })();
-
-  
